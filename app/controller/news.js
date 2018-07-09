5aa0e41f161b4be0481871efe4fd02582c7f1576
shengliang74 <shengliang74@yeah.net>
2018-07-09 22:03:50 +0800

    middleware

--- app/controller/news.js
+++ app/controller/news.js
@@ -4,10 +4,11 @@ class NewsController extends Controller {
 	async list(){
 		const ctx = this.ctx;
 		const page = ctx.query.page || 1;
-		console.log("222222222222222222222222222222222",page);
-		const newsList = await ctx.service.news.list(page);
-		console.log("55555555555555555555555555555555555",newsList);
-		await ctx.render('news/list.tpl', {list: newsList});
+		// console.log("222222222222222222222222222222222",page);
+		// const newsList = await ctx.service.news.list(page);
+		// console.log("55555555555555555555555555555555555",newsList);
+		// await ctx.render('news/list.tpl', {list: newsList});
+		this.ctx.body = 'Hello world2';
 	}
 }
 
--- app/middleware/robot.js
+++ app/middleware/robot.js
@@ -16,12 +16,16 @@
 module.exports = (options, app) => {
   return async function robotMiddleware(ctx, next) {
     const source = ctx.get('user-agent') || '';
+    console.log("1111111111111111111baidu");
+    console.log(options,source);
     const match = options.ua.some(ua => ua.test(source));
     if(match) {
       ctx.status = 403;
       ctx.message = 'Go away, robot.';
     }else{
       await next();
+      ctx.status = 403;
+      ctx.message = 'Go away, robot.aaa';
     }
   }
 }
